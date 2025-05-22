5bbf19e024b5d48b36181f08697c2b4aa9ce4ca9
capt-farvez <farvez.capt@gmail.com>
2025-05-22 19:52:39 +0600

    Add User model with BloodGroup enum and properties for user details

---
+++ LifeBridge/Models/UserModel.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LifeBridge.Models
+{
+    public enum BloodGroup
+    {
+        [Display(Name = "A+")]
+        A_Positive,
+        [Display(Name = "A-")]
+        A_Negative,
+        [Display(Name = "B+")]
+        B_Positive,
+        [Display(Name = "B-")]
+        B_Negative,
+        [Display(Name = "AB+")]
+        AB_Positive,
+        [Display(Name = "AB-")]
+        AB_Negative,
+        [Display(Name = "O+")]
+        O_Positive,
+        [Display(Name = "O-")]
+        O_Negative
+    }
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public BloodGroup BloodGroup { get; set; }
+    }
+}
