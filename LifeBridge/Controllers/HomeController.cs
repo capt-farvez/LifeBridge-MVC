68f0afef6210e55ade7404029203b13b464a177f
capt-farvez <farvez165@gmail.com>
2025-05-24 20:11:42 +0600

    Add AccessDenied action and view to handle unauthorized access

--- LifeBridge/Controllers/HomeController.cs
+++ LifeBridge/Controllers/HomeController.cs
@@ -36,4 +36,10 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    [Route("access-denied")]
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
 }
